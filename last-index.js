248809d0e726d67f1fc25ab7987aec483a283bea
sylvielcq <slcq.dev@gmail.com>
2022-07-13 16:08:38 -0700

    Function determining the last index of an item in an array

---
+++ last-index.js
@@ -0,0 +1,19 @@
+function lastIndexOf(array, value) {
+  let valueIndex;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      valueIndex = i;
+    }
+  }
+  if (valueIndex === undefined) {
+    valueIndex = -1; 
+  }
+  return valueIndex;
+}
+  
+
+  console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+  console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+  console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+  console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+  console.log(lastIndexOf([], 3), "=?", -1);
